215dc0c439b6f887307f53cd1b03e624c10aa6a7
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2022-01-05 12:07:39 +0100

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PDF_merger [![Build Status](https://github.com/scheidan/PDF_merger.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/scheidan/PDF_merger.jl/actions/workflows/CI.yml?query=branch%3Amain) [![Coverage](https://codecov.io/gh/scheidan/PDF_merger.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/scheidan/PDF_merger.jl)
+# PDF_merger.jl
+
+[![Build Status](https://github.com/scheidan/PDF_merger.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/scheidan/PDF_merger.jl/actions/workflows/CI.yml?query=branch%3Amain) [![Coverage](https://codecov.io/gh/scheidan/PDF_merger.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/scheidan/PDF_merger.jl)
+
+
+
+A simple tool to merge pdf files on all platforms.
--- src/PDF_merger.jl
+++ src/PDF_merger.jl
@@ -1,5 +1,43 @@
 module PDF_merger
 
-# Write your package code here.
+import Base.Filesystem
+using Poppler_jll: pdfunite
+
+export merge_pdfs, append_pdf!
+
+function merge_pdfs(files::Vector, destination="merged.pdf"; cleanup=false)
+    if destination in files
+        # rename existing file
+        Filesystem.mv(destination, "_x_" * destination)
+        files[files .== destination] .= "_x_" * destination
+    end
+
+    # merge pdf
+    pdfunite() do unite
+        run(`$unite $files $destination`)
+    end
+
+    # remove temp files
+    Filesystem.rm("_x_" * destination)
+    if cleanup
+        Filesystem.rm.(files, force=true)
+    end
+
+end
+
+
+function append_pdf!(file1, file2; create=true, cleanup=false)
+    if Filesystem.isfile(file1)
+        merge_pdfs([file1, file2], file1, cleanup=cleanup)
+    else
+        create || error("File '$file1' does not exist!")
+        if cleanup
+            Filesystem.mv(file2, file1)
+        else
+            Filesystem.cp(file2, file1)
+        end
+    end
+end
+
 
 end
